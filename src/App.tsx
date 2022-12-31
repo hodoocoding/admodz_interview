a28b13fb85b42806b5979d73f2015c80860f8d5b
yunsung <yunsung@marvrus.com>
2023-01-01 00:12:51 +0900

    chore: App.tsx 파일 초기 세팅구성 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import "./App.css";
+import { ProductAddForm } from "components/productAddForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>아몬즈 인터뷰 프로젝트</header>
+      <ProductAddForm />
     </div>
   );
 }
