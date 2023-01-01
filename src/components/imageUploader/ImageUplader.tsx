ea6c97e3b95bac2522a67bb0cfd77dee13a3a6f2
yunsung <yunsung@marvrus.com>
2023-01-01 20:11:07 +0900

    feat/ImageUploader: 이미지 업로드 컴포넌트 추가

---
+++ src/components/imageUploader/ImageUplader.tsx
@@ -0,0 +1,71 @@
+import React, { ChangeEvent, useState, useRef } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import * as Styled from "./ImageUploader.style";
+
+type ImageUploaderProp = {
+  setProductThumbnail: (image: string) => void;
+};
+
+const PLACEHOLDER_IMG =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN8/x8AAuMB8DtXNJsAAAAASUVORK5CYII=";
+
+function ImageUploader(props: ImageUploaderProp) {
+  const { setProductThumbnail } = props;
+  const [image, setImage] = useState<string | null>(); // 미리보기 이미지
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  const handleUpdate = (e: ChangeEvent<HTMLInputElement>) => {
+    const target = e.currentTarget;
+    const files = (target.files as FileList)[0];
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (reader) {
+        setImage(reader.result as string);
+        setProductThumbnail(reader.result as string);
+      }
+      e.target.value = "";
+    };
+    reader.readAsDataURL(files);
+  };
+
+  return (
+    <Styled.Container>
+      {image ? (
+        <Styled.ImageWraper>
+          <Styled.CloseButtonWrap
+            onClick={() => {
+              setImage(null);
+              setProductThumbnail("");
+            }}
+          >
+            <AiOutlineClose />
+          </Styled.CloseButtonWrap>
+          <Styled.Image src={image || PLACEHOLDER_IMG} />
+        </Styled.ImageWraper>
+      ) : null}
+      <Styled.Label>
+        <Styled.Button onClick={() => handleButtonClick()}>
+          이미지 업로드
+          <input
+            accept="image/*"
+            type="file"
+            id="fileUpload"
+            ref={inputRef}
+            onChange={handleUpdate}
+            style={{ display: "none" }}
+          />
+        </Styled.Button>
+      </Styled.Label>
+    </Styled.Container>
+  );
+}
+
+export default ImageUploader;
