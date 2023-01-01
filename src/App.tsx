dbb388de07caf53a640762354ae6f625f1717726
yunsung <yunsung@marvrus.com>
2023-01-02 00:16:30 +0900

    fix/main: react-router-dom 경로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProductAddForm } from "components/productAddForm";
+import { ProductList } from "components/productList";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header>아몬즈 인터뷰 프로젝트</header>
-      <ProductAddForm />
+      <BrowserRouter>
+        <header>아몬즈 인터뷰 프로젝트</header>
+        <Routes>
+          <Route path="/" element={<ProductAddForm />} />
+          <Route path="/products" element={<ProductList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
