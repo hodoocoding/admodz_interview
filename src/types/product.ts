650ada596decf8f162536951effdcb1e4a24e8cd
yunsung <yunsung@marvrus.com>
2023-01-05 13:42:21 +0900

    fix/type: 상품 타입 수정

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,6 +1,7 @@
-export type Product = {
-  id: string;
+export type ProductType = {
+  id: number | string;
   name: string;
   thumbnail: string | null;
-  price: number;
+  price: number | string;
+  category?: string;
 };
